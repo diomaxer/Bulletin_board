80c9e7d7c6360e115aae7eff728402c2ce6b6c9c
Zharkyn20 <61348606+Zharkyn20@users.noreply.github.com>
2022-08-18 12:20:25 +0600

    add gitignore, add user and advertisement api

--- advertisement/views.py
+++ advertisement/views.py
@@ -1,62 +1,31 @@
-from django.shortcuts import render, redirect
-from django.views.generic import DetailView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .models import Advertisement, AdvertisementImage
-from .forms import AdvertisementForm
-from django.contrib.auth.decorators import login_required
+from .serializers import AdvertisementSerializer
+from ..advertisement.models import Advertisement
 
+class AdvertisementView(APIView):
+    """Advertisement API View"""
 
-def index(request):
-    """Home page"""
-    ads = Advertisement.objects.all().order_by('-id')[:10]
-    context = {
-        'ads': ads,
-    }
-    return render(request, 'index.html', context)
+    def get(self, request):
+        ads = Advertisement.objects.all()
+        serializer = AdvertisementSerializer(ads, many=True)
+        return Response({'ads': serializer.data})
 
 
-def index_detail(request, pk):
-    """Advertisement detail view"""
-    ad = Advertisement.objects.get(id=pk)
-    images = AdvertisementImage.objects.filter(advertisement=ad.id)
-    context = {
-        'ad': ad,
-        'images': images,
-    }
-    return render(request, 'index_detail.html', context)
+class AdvertisementDetailView(APIView):
+    """Advertisement API View"""
+
+    def get(self, request, pk):
+        ad = Advertisement.objects.get(id=self.pk)
+        serializer = AdvertisementSerializer(ad)
+        return Response({'ad': serializer.data})
 
 
-class IndexDetail(DetailView):
-    model = Advertisement
-    template_name = 'index_detail.html'
-    context_object_name = 'ad'
+class HomeAdvertisementView(APIView):
+    """Advertisement API View for home page"""
 
-
-
-@login_required
-def create_ad_view(request):
-    """Create new advertisement view"""
-    if request.method == 'POST':
-        form = AdvertisementForm(request.POST)
-        images = request.FILES.getlist('images')
-        if form.is_valid():
-            new_ad = Advertisement.objects.create(
-                title=form.cleaned_data['title'],
-                user=request.user,
-                description=form.cleaned_data['description'],
-                head_image=images[0]
-            )
-            images.pop(0)
-            if len(images) != 0:
-                for image in images:
-                    AdvertisementImage.objects.create(
-                        advertisement=new_ad,
-                        image=image
-                    )
-            return redirect('home')
-    else:
-        form = AdvertisementForm()
-    context = {
-        'form': form,
-    }
-    return render(request, 'advertisement/advertisement_create.html', context)
+    def get(self, request):
+        ads = Advertisement.objects.all()[:10]
+        serializer = AdvertisementSerializer(ads, many=True)
+        return Response({'ads': serializer.data})
